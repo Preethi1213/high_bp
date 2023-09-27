c61fcdb2e9f1f9b513e525fcc4774034cb4b6c18
Preethi1213 <128666027+Preethi1213@users.noreply.github.com>
2023-09-28 04:42:16 +0530

    Update deploy_hbp_py.py

--- deploy_hbp_py.py
+++ deploy_hbp_py.py
@@ -21,8 +21,27 @@ st.image(image, use_column_width=True)
 
 age = st.number_input("Age of a person",min_value=0,max_value=100,value=0)
 height = st.number_input("Height of a person",min_value=0,max_value=400,value=0)
-
-
+weight=st.number_input("weight of a person",min_value=0,max_value=1000,value=0)
+waist=st.number_input("waist size",min_value=0,max_value=1000,value=0)
+eyesight=st.number_input("Eyesight(left)",min_value=0,max_value=1000,value=0)
+eyesight=st.number_input("Eyesight(right)",min_value=0,max_value=1000,value=0)
+hearing=st.number_input("Hearing(left)",min_value=0,max_value=1000,value=0)
+hearing=st.number_input("Hearing(right)",min_value=0,max_value=1000,value=0)
+systolic=st.number_input("Systolic pressure",min_value=0,max_value=1000,value=0)
+relaxation=st.number_input("Dyastolic pressure",min_value=0,max_value=1000,value=0)
+fasting blood sugar=st.number_input("Blood sugar",min_value=0,max_value=1000,value=0)
+Cholesterol=st.number_input("Cholesterol",min_value=0,max_value=1000,value=0)
+triglyceride=st.number_input("Triglyceride",min_value=0,max_value=1000,value=0)
+HDL=st.number_input("HDL",min_value=0,max_value=1000,value=0)
+LDL=st.number_input("LDL",min_value=0,max_value=1000,value=0)
+hemoglobin=st.number_input("Hemoglobin",min_value=0,max_value=1000,value=0)
+Urine protein=st.number_input("urine protein",min_value=0,max_value=1000,value=0)
+serum creatinine=st.number_input("serum creatinine",min_value=0,max_value=1000,value=0)
+AST=st.number_input("AST",min_value=0,max_value=1000,value=0)
+ALT=st.number_input("ALT",min_value=0,max_value=1000,value=0)
+Gtp=st.number_input("GTP",min_value=0,max_value=1000,value=0)
+dental caries=st.number_input("dental caries",min_value=0,max_value=1000,value=0)
+smoking=st.number_input("smoking",min_value=0,max_value=100,value=0)
 if st.button("HyperTension test result"):
     input_features = [[age, height]] 
     prediction = model.predict(input_features)[0]
