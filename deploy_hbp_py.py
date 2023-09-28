6567b3f328721cf63a9be04f7d9d2018548ecf76
Preethi1213 <128666027+Preethi1213@users.noreply.github.com>
2023-09-28 06:02:30 +0530

    Update deploy_hbp_py.py

--- deploy_hbp_py.py
+++ deploy_hbp_py.py
@@ -23,31 +23,18 @@ age = st.number_input("Age of a person",min_value=0,max_value=100,value=0)
 height = st.number_input("Height of a person",min_value=0,max_value=400,value=0)
 weight=st.number_input("weight of a person",min_value=0,max_value=1000,value=0)
 waist=st.number_input("waist size",min_value=0,max_value=1000,value=0)
-eyesight=st.number_input("Eyesight(left)",min_value=0.0)
-eyesight=st.number_input("Eyesight(right)",min_value=0.0)
-hearing=st.number_input("Hearing(left)",min_value=0,max_value=1000,value=0)
-hearing=st.number_input("Hearing(right)",min_value=0,max_value=1000,value=0)
 systolic=st.number_input("Systolic pressure",min_value=0,max_value=1000,value=0)
 relaxation=st.number_input("Dyastolic pressure",min_value=0,max_value=1000,value=0)
 fasting_blood_sugar=st.number_input("Blood sugar",min_value=0,max_value=1000,value=0)
 Cholesterol=st.number_input("Cholesterol",min_value=0,max_value=1000,value=0)
-triglyceride=st.number_input("Triglyceride",min_value=0,max_value=1000,value=0)
 HDL=st.number_input("HDL",min_value=0,max_value=1000,value=0)
 LDL=st.number_input("LDL",min_value=0,max_value=1000,value=0)
 hemoglobin=st.number_input("Hemoglobin",min_value=0.0)
-Urine_protein=st.number_input("urine protein",min_value=0,max_value=1000,value=0)
-serum_creatinine=st.number_input("serum creatinine",min_value=0.0)
-AST=st.number_input("AST",min_value=0,max_value=1000,value=0)
-ALT=st.number_input("ALT",min_value=0,max_value=1000,value=0)
-Gtp=st.number_input("GTP",min_value=0,max_value=1000,value=0)
-dental_caries=st.number_input("dental caries",min_value=0,max_value=1000,value=0)
 smoking=st.number_input("smoking",min_value=0,max_value=100,value=0)
 if st.button("HyperTension test result"):
-    input_features = [[age, height,weight,waist,eyesight,
-       eyesight,hearing,hearing,systolic,
-       relaxation, fasting_blood_sugar,Cholesterol, triglyceride,
-       HDL,LDL,hemoglobin, Urine_protein,serum_creatinine,AST,
-       ALT,Gtp,dental_caries,smoking]] 
+    input_features = [[age, height,weight,waist,systolic,
+       relaxation, fasting_blood_sugar,Cholesterol,
+       HDL,LDL,hemoglobin,smoking]] 
     prediction = model.predict(input_features)[0]
     if prediction == 0:
             st.subheader("The person doesn't have Hypertension.")
@@ -56,8 +43,6 @@ if st.button("HyperTension test result"):
 else:
         st.subheader("Please enter a text for prediction.")
 
-st.write("Debug: Input Features Shape:", len(input_features))
-st.write("Debug: Input Features:", input_features)
 
 
 prediction = model.predict(input_features)[0]
